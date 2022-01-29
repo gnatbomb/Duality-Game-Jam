a4aed32a259a30f7e6a052aec614c40add2a8877
gnatbomb <nbombard@ualberta.ca>
2022-01-28 21:26:58 -0700

    added menu

---
+++ Duality/Scenes/BackButton.gd
@@ -0,0 +1,17 @@
+extends VBoxContainer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+
+func _on_MainMenuButton_pressed():
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
