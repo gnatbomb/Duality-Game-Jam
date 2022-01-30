fa4f449d728fd17223ae1da844ef0261e4eca186
gnatbomb <nbombard@ualberta.ca>
2022-01-29 23:53:02 -0700

    pushing

--- Duality/Scripts/Player1.gd
+++ Duality/Scripts/Player1.gd
@@ -2,3 +2,6 @@ extends "res://Scripts/Player.gd"
 
 func _setControls():
 	controlDict = {"up":"P1up","swap":"P1down","left":"P1left","right":"P1right"}
+
+func _on_Area2D_body_entered(body):
+	print("hewwo")
