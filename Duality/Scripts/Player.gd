e9ca4aa81106335e3e67a2c72553a19fb44e4f51
Yalmaz Abdullah <42461443+YalmazAbdullah@users.noreply.github.com>
2022-01-30 04:45:32 -0700

    More stuff
    -death handling:
    1.death anim
    2.death pause
    -dresser swap

--- Duality/Scripts/Player.gd
+++ Duality/Scripts/Player.gd
@@ -12,6 +12,7 @@ export (float,0,1) var friction = 0.325
 export var terminalVelocity = 250
 export var partnerPath:NodePath
 export (int) var playernum
+export var DresserPath:NodePath
 
 #GLOBALS
 var velocity = Vector2.ZERO
@@ -22,7 +23,7 @@ var jumpOnCooldown = false
 var victory = false
 var controlDict = {}
 onready var partner = get_node(partnerPath)
-onready var startPosition = get_parent().find_node("Dresser1").position
+onready var dresser = get_node(DresserPath)
 var alive = true
 
 func _ready():
@@ -31,6 +32,8 @@ func _ready():
 func _process(delta):
 	if(alive):
 		inputHandler(delta)
+	else:
+		death()
 	return
 
 #set controls
@@ -95,27 +98,40 @@ func swap():
 	partner.position = temp
 	print(partner.position,self.position)
 	
+	#swap dressers
+	temp = self.dresser.position
+	self.dresser.position = partner.dresser.position
+	partner.dresser.position = temp
+	
+	
 	MusicController.play_SE(self.playernum, "swap")
 
 #take logic of respawn out and into game manager!
 func hitSpike():
-#	MusicController.play_SE(self.playernum, "owie")
-#	MusicController.play_SE(self.playernum, "death")
-#	alive = false
-	animationState.travel("Death")
-	print(animationState.get_current_node())
+	MusicController.play_SE(self.playernum, "owie")	
+	#MusicController.play_SE(self.playernum, "death")
+	alive = false
 	print("died")
-#	position = startPosition
 
 func hitSpring():
-	animationState.travel("Jump")
+#	animationState.travel("Jump")
+	jumpOnCooldown = true
 	velocity.y = -boostForce
 	MusicController.play_SE(self.playernum, "spring")
 	
 func win():
 	victory = true
 	
-	
+func death():
+	animationState.travel("Death")
+	print(animationState.get_current_node())
+	yield(get_tree().create_timer(0.6),"timeout")
+	#NAT LOOK HERE!!! ^ above sets a timer that godot waits for right now the timer is the length of the death anim which is 0.6
+	#add the audio stuff here
+
+	position = dresser.position
+	animationState.travel("Idle")
+	alive = true
 	
 	
 	
