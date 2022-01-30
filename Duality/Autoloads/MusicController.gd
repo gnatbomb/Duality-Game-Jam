f452b6d92a39be7859524f7464bca9c8305b930b
gnatbomb <nbombard@ualberta.ca>
2022-01-29 20:38:07 -0700

    added sound effects

--- Duality/Autoloads/MusicController.gd
+++ Duality/Autoloads/MusicController.gd
@@ -1,11 +1,72 @@
 extends Node
 
+# music load calls
+var bgm = load("res://Music/LoversLost.mp3")
 
-var bgm = load("res://Audio/LoversLost.ogg")
+# SE load calls
+var death = load("res://SE/DeathScream.ogg")
+var steppy = load("res://SE/Footsteps.ogg")
+var jump = load("res://SE/Jump.ogg")
+var land = load("res://SE/Landing.ogg")
+var embrace = load("res://SE/LoversEmbrace.ogg")
+var close = load("res://SE/MenuClose.ogg")
+var open = load("res://SE/MenuOpen.ogg")
+var respawn = load("res://SE/Respawn.ogg")
+var owie = load("res://SE/SpikeTouchYEOWCH.ogg")
+var spring = load("res://SE/Spring.ogg")
+var swap = load("res://SE/Swap.ogg")
+
+
+# SE dict
+var sounds = {
+	"death": death,
+	"steppy": steppy,
+	"jump": jump,
+	"land": land,
+	"embrace": embrace,
+	"close": close,
+	"open": open,
+	"respawn": respawn,
+	"owie": owie,
+	"spring": spring,
+	"swap": swap
+}
+
+# Audioplayer array
+var AudioPlayers = []
+
+var pitchdif = 0.1
+
+# plays a given sound effect with pitch shift based on player. Call with player = 0 for no pitch shift
+func play_SE(player, noise):
+
+	var pitchscale = 1
+	if (player == 1):
+		pitchscale -= pitchdif
+	elif (player == 2):
+		pitchscale += pitchdif
+		
+	
+	for n in AudioPlayers.size():
+		if (! AudioPlayers[n].is_playing()):
+			AudioPlayers[n].set_pitch_scale(pitchscale)
+			AudioPlayers[n].stream = sounds[noise]
+			AudioPlayers[n].play()
+			break
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AudioPlayers.append($SE1)
+	AudioPlayers.append($SE2)
+	AudioPlayers.append($SE3)
+	AudioPlayers.append($SE4)
+	AudioPlayers.append($SE5)
+	AudioPlayers.append($SE6)
+	AudioPlayers.append($SE7)
+	AudioPlayers.append($SE8)
+	AudioPlayers.append($SE9)
+	AudioPlayers.append($SE10)
 
 
 func play_music():
@@ -18,3 +79,5 @@ func default_volume():
 
 func lower_volume():
 	$Music.volume_db = -12
+	
+
