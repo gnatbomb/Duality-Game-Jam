b87c53f9ee13a5082e6138d1fc4fa0e97015a80d
gnatbomb <nbombard@ualberta.ca>
2022-01-30 00:34:24 -0700

    win noise

--- Duality/Scripts/Player1.gd
+++ Duality/Scripts/Player1.gd
@@ -4,4 +4,5 @@ func _setControls():
 	controlDict = {"up":"P1up","swap":"P1down","left":"P1left","right":"P1right"}
 
 func _on_Area2D_body_entered(body):
-	print("hewwo")
+	if body.get_name() == "Player2":
+		self.win()
