42377371ad527ef0bbdeeb3601f0f9c82bfa1c0d
Yalmaz Abdullah <42461443+YalmazAbdullah@users.noreply.github.com>
2022-01-29 22:03:30 -0700

    spring works ill add anim next

--- Duality/Scripts/Spring.gd
+++ Duality/Scripts/Spring.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+func _on_Area2D_body_entered(body):
+	print("hi")
+	if body.get_name() == "Player1" or body.get_name() == "Player2":
+		body.hitSpring()
+
 #func _on_Springs_body_entered(body):
 #	if body.get_name() == "Player":
 #		body.hitSpring()
